4712853defacf9a1d004fef3cb9d62146a7eea67
Harish Gajjela <harishgajjela123@gmail.com>
2025-06-24 21:09:04 +0530

    Reporting and Local Parallel code

---
+++ AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using NUnit.Framework;
+
+#if RUN_PARALLEL
+[assembly: Parallelizable(ParallelScope.Scenarios)]
+[assembly: LevelOfParallelism(5)]
+#else
+[assembly: Parallelizable(ParallelScope.None)]
+#endif
